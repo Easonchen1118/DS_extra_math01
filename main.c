5d69c486f74c897ae724a6ea83f1394be866d0b8
Easonchen1118 <120409122+Easonchen1118@users.noreply.github.com>
2022-12-27 18:19:29 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//計算梯型面積
+//計算梯型面積https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=6855
 int main(){
     int up, down, height;
     while (scanf("%d %d %d", &up, &down, &height) != EOF) {
